85c025e2deeae5fe7b3e9f5bfcf8750b10d7cbd4
ubuntu24.04.1-Vmware <154095742@qq.com>
2024-11-06 18:08:44 +0800

    system

--- system/fork_fd_test.c
+++ system/fork_fd_test.c
@@ -9,10 +9,43 @@ int main(int argc, char const *argv[])
 {
     
     int fd = open("io.txt", O_CREAT | O_WRONLY | O_APPEND, 0664);
-    if (fd)
+    if (fd == -1)
     {
-        /* code */
+        perror("open");
+        exit(EXIT_FAILURE);
     }
+    char buffer[1024];
+
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        perror("fail to fork");
+    } else if (pid == 0) {
+        //sub process
+        strcpy(buffer, "这是子进程写的数据\n");
+    } else {
+        // parent process
+        sleep(1);
+        strcpy(buffer, "这是父进程写的数据\n");
+    }
+    // 父子进程都要执行的代码
+    ssize_t bytes_write = write(fd, buffer, strlen(buffer));
+    if (bytes_write == -1)
+    {
+        perror("fail to write");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    close(fd);
+    if (pid == 0)
+    {
+        printf("子进程写入完毕\n");
+    } else {
+        printf("父进程写入完毕\n");
+    }
+    
+    
     
     return 0;
 }
